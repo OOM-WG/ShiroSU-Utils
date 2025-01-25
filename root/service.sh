c53b86b4f6bea343f8058d8acfbbde43bd3daf89
梦璃酱 <yumeyuka@oom-wg.dev>
2025-01-25 11:52:45 +0800

    fix

--- root/service.sh
+++ root/service.sh
@@ -6,12 +6,12 @@ baseDir="$(dirname "$(readlink -f "$0")")"
 # code before boot completed
 
 until_boot 90
-nohup_bin "$baseDir/bin/Intelligent" 
+nohup_bin "$baseDir/Intelligent" 
 
 # code after boot completed and before unlocked
 
 # run boot-completed.sh if it exists and root is not supported by KernelSU or APatch, then other code in this script will not run
-run_boot_completed_if_magisk "$baseDir"
+# run_boot_completed_if_magisk "$baseDir"
 
 until_unlock
 
