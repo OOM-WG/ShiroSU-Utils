113daa721644355cfb0d85ccac036369436548fe
白彩恋 <shiro@oom-wg.dev>
2025-02-07 13:14:22 +0800

    优化

--- root/service.sh
+++ root/service.sh
@@ -3,18 +3,6 @@
 baseDir="$(dirname "$(readlink -f "$0")")"
 [ -f "$baseDir/skt-utils.sh" ] && . "$baseDir/skt-utils.sh" || exit
 
-# code before boot completed
-
 until_boot 90
-nohup_bin "$baseDir/intelligent" 
 
-# code after boot completed and before unlocked
-
-# run boot-completed.sh if it exists and root is not supported by KernelSU or APatch, then other code in this script will not run
-# run_boot_completed_if_magisk "$baseDir"
-
-until_unlock
-
-# code after unlocked
-
-
+nohup_bin "$baseDir/intelligent"
