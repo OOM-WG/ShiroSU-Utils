bcc2671137c3cf584d3e188e7a4159dfe065bc07
白彩恋 <shiro@oom-wg.dev>
2025-02-09 17:29:44 +0800

    修复卸载堵塞问题

--- root/uninstall.sh
+++ root/uninstall.sh
@@ -1,6 +1,7 @@
 baseDir="$(dirname "$(readlink -f "$0")")"
 [ -f "$baseDir/skt-utils.sh" ] && . "$baseDir/skt-utils.sh" || exit
 
-until_unlock 5
-
-del -rf /sdcard/Android/Intelligent
+{
+  until_unlock 5
+  del -rf /sdcard/Android/Intelligent
+} &
