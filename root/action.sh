0f25aa13fd1b28bd9a13fe151b815005d7b48a75
白彩恋 <shiro@oom-wg.dev>
2025-03-09 09:17:39 +0800

    版权声明

--- root/action.sh
+++ root/action.sh
@@ -1,3 +1,19 @@
+#=================================================================================================================
+# Copyright (c) 2023-present Anne Sakitin (Tianwan Ayana) & YumeYuka.                                            =
+#                                                                                                                =
+# Part of the SUU project.                                                                                       =
+# Part of the NGA project.                                                                                       =
+# Licensed under the F2DLPR License.                                                                             =
+#                                                                                                                =
+# YOU MAY NOT USE THIS FILE EXCEPT IN COMPLIANCE WITH THE LICENSE.                                               =
+# Provided "AS IS", WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,                                                =
+# unless required by applicable law or agreed to in writing.                                                     =
+#                                                                                                                =
+# For details about the SUU project, visit: https://www.mod.latestfile.zip.                                      =
+# For details about the NGA project, visit: http://app.niggergo.work.                                            =
+# For details about the F2DLPR License terms and conditions, visit: http://license.fileto.download.              =
+#=================================================================================================================
+
 baseDir="$(dirname "$(readlink -f "$0")")"
 [ -f "$baseDir/skt-utils.sh" ] && . "$baseDir/skt-utils.sh" || exit
 
--- root/customize.sh
+++ root/customize.sh
@@ -1,3 +1,19 @@
+#=================================================================================================================
+# Copyright (c) 2023-present Anne Sakitin (Tianwan Ayana) & YumeYuka.                                            =
+#                                                                                                                =
+# Part of the SUU project.                                                                                       =
+# Part of the NGA project.                                                                                       =
+# Licensed under the F2DLPR License.                                                                             =
+#                                                                                                                =
+# YOU MAY NOT USE THIS FILE EXCEPT IN COMPLIANCE WITH THE LICENSE.                                               =
+# Provided "AS IS", WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,                                                =
+# unless required by applicable law or agreed to in writing.                                                     =
+#                                                                                                                =
+# For details about the SUU project, visit: https://www.mod.latestfile.zip.                                      =
+# For details about the NGA project, visit: http://app.niggergo.work.                                            =
+# For details about the F2DLPR License terms and conditions, visit: http://license.fileto.download.              =
+#=================================================================================================================
+
 [ -f "$MODPATH/skt-utils.sh" ] && . "$MODPATH/skt-utils.sh" || abort '! File "skt-utils.sh" does not exist!'
 skt_install_init "config.fvv" # Don't write code before this line!
 
