1d0d333cf59fb069257bee8d1cb812f5947f4a96
白彩恋 <shiro@oom-wg.dev>
2025-04-19 17:54:27 +0800

    优化版权声明

--- root/customize.sh
+++ root/customize.sh
@@ -1,18 +1,18 @@
-#=================================================================================================================
-# Copyright (c) 2023-present Anne Sakitin (Tianwan Ayana) & YumeYuka.                                            =
-#                                                                                                                =
-# Part of the SUU project.                                                                                       =
-# Part of the NGA project.                                                                                       =
-# Licensed under the F2DLPR License.                                                                             =
-#                                                                                                                =
-# YOU MAY NOT USE THIS FILE EXCEPT IN COMPLIANCE WITH THE LICENSE.                                               =
-# Provided "AS IS", WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,                                                =
-# unless required by applicable law or agreed to in writing.                                                     =
-#                                                                                                                =
-# For details about the SUU project, visit: https://www.mod.latestfile.zip.                                      =
-# For details about the NGA project, visit: http://app.niggergo.work.                                            =
-# For details about the F2DLPR License terms and conditions, visit: http://license.fileto.download.              =
-#=================================================================================================================
+#====================================================================================================
+# Copyright (c) 2023-present Anne Sakitin (Tianwan Ayana) & YumeYuka.                               =
+#                                                                                                   =
+# Part of the SUU project.                                                                          =
+# Part of the NGA project.                                                                          =
+# Licensed under the F2DLPR License.                                                                =
+#                                                                                                   =
+# YOU MAY NOT USE THIS FILE EXCEPT IN COMPLIANCE WITH THE LICENSE.                                  =
+# Provided "AS IS", WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,                                   =
+# unless required by applicable law or agreed to in writing.                                        =
+#                                                                                                   =
+# For details about the SUU project, visit: https://www.mod.latestfile.zip/suu.                     =
+# For details about the NGA project, visit: http://app.niggergo.work.                               =
+# For details about the F2DLPR License terms and conditions, visit: http://license.fileto.download. =
+#====================================================================================================
 
 [ -f "$MODPATH/nga-utils.sh" ] && . "$MODPATH/nga-utils.sh" || abort '! File "nga-utils.sh" does not exist!'
 nga_install_init "config.fvv" # Don't write code before this line!
