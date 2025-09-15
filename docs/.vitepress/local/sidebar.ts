9ed1f45abd3a6c24b176072464df0652e92eaa1a
梦璃酱 <Miao@YumeYuka.plus>
2025-09-15 23:51:28 +0800

    Update sidebar.ts

--- docs/.vitepress/local/sidebar.ts
+++ docs/.vitepress/local/sidebar.ts
@@ -15,24 +15,13 @@ export const sidebar: DefaultTheme.Config['sidebar'] = [
 		text: '功能',
 		collapsed: false,
 		items: [
-			{
-				text: 'Android',
-				collapsed: true,
-				items: [
 					{text: 'Nyanya整理', link: '/Feat/Android/Nyanya'},
-					{text: 'C&C清理', link: '/Feat/Android/C&C'},
-					{text: 'GBB优质存储', link: '/Feat/Android/GBB'},
+					{text: 'C&C清理'， link: '/Feat/Android/C&C'},
+					{text: 'GBB优质存储'， link: '/Feat/Android/GBB'},
 					{text: 'modify.my.id', link: '/Feat/Android/modify.my.id'},
 					{text: 'Xposed功能', link: '/Feat/Android/Xposed'},
 					{text: 'Toybox玩具箱', link: '/Feat/Android/Toybox'}
 				]
-			},
-			{
-				text: 'Windows',
-				collapsed: true,
-				items: []
-			}
-		]
 	},
 	{
 		text: '更新日志',
@@ -46,20 +35,9 @@ export const sidebar: DefaultTheme.Config['sidebar'] = [
 		text: '附录',
 		collapsed: false,
 		items: [
-			{
-				text: 'Android',
-				collapsed: true,
-				items: [
 					{text: '模式对照', link: '/Appendix/Android/Mode'},
-					{text: '路径对照', link: '/Appendix/Android/Path'},
-					{text: '小贴士', link: '/Appendix/Android/Tip'}
+					{text: '路径对照'， link: '/Appendix/Android/Path'},
+					{text: '小贴士'， link: '/Appendix/Android/Tip'}
 				]
-			},
-			{
-				text: 'Windows',
-				collapsed: true,
-				items: []
-			}
-		]
 	}
 ]
